af04b8e2f97f051053a99fb6ad3a249a706a79b6
Taiwo Joseph <60525931+taiwojay@users.noreply.github.com>
2020-06-20 19:44:22 +0100

    update app.js

--- App.js
+++ App.js
@@ -3,18 +3,17 @@ import './App.css';
 
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-import Grid from './components/Grid/Grid';
-import Nav from './components/Nav/Nav';
-//import Cover from './components/Cover/Cover';
-import Footer from './components/Footer/Footer';
+import Grid from './Grid';
+import Nav from './Nav';
+import Footer from './Footer';
 
 
 
-import Series from './components/categories/Series'
-import Animations from './components/categories/Animations'
-import Movies from './components/categories/Movies'
-import Games from './components/categories/Games'
-import MarvelDC from './components/categories/MarvelDC'
+import Series from './Series'
+import Animations from './Animations'
+import Movies from './Movies'
+import Games from './Games'
+import MarvelDC from './MarvelDC'
 
 
 class App extends React.Component{ n
@@ -26,15 +25,13 @@ class App extends React.Component{ n
           
            
             <Switch>
-                <Route path ='/' exact component={Grid} />
-                
+                <Route path ='/' exact component={Grid} />         
                 <Route path ='/series' exact component={Series} />
                 <Route path ='/animations' exact component={Animations} />
                 <Route path ='/movies' exact component={Movies} />
                 <Route path ='/marveldc' exact component={MarvelDC} />
-                <Route path ='/games' exact component={Games} />  
-                  
-              </Switch>
+                <Route path ='/games' exact component={Games} />      
+             </Switch>
               <Footer />
               
             </div>
